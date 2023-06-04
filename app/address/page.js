961a79ae3fd0fecd5e6afe91b7843c0f237dcf1b
Nadim <hussennadim49@gmail.com>
2023-06-04 17:55:29 +0600

    data fetch

---
+++ app/address/page.js
@@ -0,0 +1,13 @@
+
+import Link from 'next/link';
+
+export default function Address() {
+  return (
+    <>
+    <h1>
+        Address
+    </h1>
+    <Link href='/'>Home</Link>
+    </>
+  )
+}
--- app/page.js
+++ app/page.js
@@ -1,9 +1,23 @@
 import Image from 'next/image'
+import Link from 'next/link';
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts?userId=2');
 
-export default function Home() {
+  return res.json();
+}
+export default async function Home() {
+  const data = await getData();
+  const name = 'hello world';
+  console.log('hello world');
   return (
     <>
+    <Link href='/address'>Address</Link>
     <h1>Hello world</h1>
+    {data.map((item)=>{
+      return <h6>{item.title.toUpperCase()}</h6>
+
+    })}
+    <h3>{name}</h3>
     </>
   )
 }
